d77cf0ffa778fe535f219e198e694e3b9b23b648
My Le <irisle412@gmail.com>
2022-04-08 17:23:09 +0700

    detail page: add UI and link to MovieCard

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import FilterGenres from "./components/FilterGenres";
 import MovieDetails from "./pages/MovieDetails";
+import BrowsePage from "./pages/BrowsePage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<FilterGenres />} />
+        <Route path="/browser" element={<BrowsePage />} />
         <Route path="/movie/:movieId" element={<MovieDetails />} />
       </Routes>
     </>
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,14 +5,21 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 import apiConfig from "../app/apiConfig";
+import { useNavigate } from "react-router-dom";
 
 function ProductCard({ movie }) {
   const backgroundImage = apiConfig.originalImage(movie.backdrop_path);
   movie.backgroundImage = backgroundImage;
   const posterImage = apiConfig.w500Image(movie.poster_path);
   movie.posterImage = posterImage;
+  const navigate = useNavigate();
   return (
-    <Card>
+    <Card
+      onClick={() => {
+        console.log(`card`);
+        navigate(`/movie/${movie.id}`);
+      }}
+    >
       <CardActionArea>
         <CardMedia
           component="img"
