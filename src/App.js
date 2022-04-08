aa42fc1100ad14f40de00e1656cb6289056a6483
Trang Luong <luongtrang014@gmail.com>
2022-04-08 14:43:44 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import FilterGenres from "./components/FilterGenres";
+import HomePages from "./pages/HomePages";
 import MovieDetails from "./pages/MovieDetails";
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<FilterGenres />} />
-        <Route path="/movie/:movieId" element={<MovieDetails />} />
-      </Routes>
-    </>
-  );
+  return <HomePages />;
 }
 
 export default App;
