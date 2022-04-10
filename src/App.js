85afb439a97a120c3079e2c5da273565722137df
My Le <irisle412@gmail.com>
2022-04-10 11:22:37 +0700

    add navbar, login and initialize cart

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import "./App.css";
 import HomePages from "./pages/HomePages";
 import MovieDetails from "./pages/MovieDetails";
 import BrowsePage from "./pages/BrowsePage";
+import Layout from "./layouts/Layout";
+import { AuthProvider } from "./contexts/AuthContext";
+import LoginPage from "./pages/LoginPage";
+import { CartContext } from "./contexts/CartContext";
 
 function App() {
   return (
     <div className="App">
       <>
-        <Routes>
-          <Route path="/" element={<HomePages />} />
-          <Route path="/browser" element={<BrowsePage />} />
-          <Route path="/movie/:movieId" element={<MovieDetails />} />
-        </Routes>
+        <AuthProvider>
+          {/* <CartContext> */}
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<HomePages />} />
+              <Route path="/browser" element={<BrowsePage />} />
+              <Route path="/movie/:movieId" element={<MovieDetails />} />
+              <Route path="/login" element={<LoginPage />} />
+            </Route>
+          </Routes>
+          {/* </CartContext> */}
+        </AuthProvider>
       </>
     </div>
   );
