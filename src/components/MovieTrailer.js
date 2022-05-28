7e1481450e44d4ffa14b4499203554b156c2802e
XuanLe <leanhxuan051996@gmail.com>
2022-05-28 23:37:55 +0700

    The Movie DB

--- src/components/MovieTrailer.js
+++ src/components/MovieTrailer.js
@@ -19,7 +19,7 @@ const MovieTrailer = () => {
     };
 
     getTrailer();
-  }, []);
+  }, [movieId]);
 
   useEffect(() => {
     if (!trailer) {
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -31,7 +31,7 @@ const MovieDetails = () => {
       }
     };
     getMoveDetails();
-  }, []);
+  }, [movieId]);
 
   if (!movie) return <LoadingScreen />;
 
